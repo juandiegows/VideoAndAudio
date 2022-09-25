2445bd2b366792477b6b1b9ec4fa8bc4c7960983
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-09-25 15:37:58 -0500

    Add project files.

---
+++ Video/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Video
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnplaySound = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(800, 398);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            // 
+            // btnplaySound
+            // 
+            this.btnplaySound.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnplaySound.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnplaySound.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnplaySound.ForeColor = System.Drawing.Color.Green;
+            this.btnplaySound.Location = new System.Drawing.Point(653, 415);
+            this.btnplaySound.Name = "btnplaySound";
+            this.btnplaySound.Size = new System.Drawing.Size(135, 32);
+            this.btnplaySound.TabIndex = 1;
+            this.btnplaySound.Text = "Play Sound";
+            this.btnplaySound.UseVisualStyleBackColor = true;
+            this.btnplaySound.Click += new System.EventHandler(this.btnplaySound_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnplaySound);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btnplaySound;
+    }
+}
+
---
+++ Video/Form1.cs
@@ -0,0 +1,64 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Video
+{
+    public partial class Form1 : Form
+    {
+        String temp = "";
+        String tempAudio = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      
+            temp = Path.GetTempFileName();
+            File.Move(temp, temp = Path.ChangeExtension(temp, "mp4"));
+            using(FileStream fs = new FileStream(temp, FileMode.OpenOrCreate))
+            {
+                MemoryStream memoryStream = new MemoryStream(global::Video.Properties.Resources.v1);
+                memoryStream.CopyTo(fs);
+                memoryStream.Close();
+                fs.Close();
+            }
+            
+            axWindowsMediaPlayer1.URL = temp;
+          
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if(e.newState == 8)
+            {
+                File.Delete(temp);
+            }
+        }
+
+        private void btnplaySound_Click(object sender, EventArgs e)
+        {
+            tempAudio = Path.GetTempFileName();
+            File.Move(tempAudio, tempAudio = Path.ChangeExtension(tempAudio, "mp3"));
+            using(FileStream fs = new FileStream(tempAudio, FileMode.OpenOrCreate))
+            {
+                Properties.Resources.som3.CopyTo(fs);
+                fs.Close();
+            }
+            SoundPlayer sp = new SoundPlayer(tempAudio);
+            sp.Play();
+
+        }
+    }
+}
